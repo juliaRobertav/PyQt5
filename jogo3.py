fc946efbd009514960dc94f322152da146a89468
julinha <114812014+juliaRobertav@users.noreply.github.com>
2023-06-21 10:29:22 -0300

    Create jogo3.py

---
+++ jogo3.py
@@ -0,0 +1,63 @@
+import sys
+import random
+from PyQt5.QtGui import QFont
+from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget, QLineEdit, QPushButton, QLabel
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.label = QLabel("Escolha uma opção:")
+        self.label.setFont(QFont("Arial", 12))
+
+        layout = QVBoxLayout()
+
+        self.line_edit = QLineEdit()
+        self.button = QPushButton("JOGAR")
+
+        layout.addWidget(self.label)
+        layout.addWidget(self.line_edit)
+        layout.addWidget(self.button)
+
+        widget = QWidget()
+        widget.setLayout(layout)
+        self.setCentralWidget(widget)
+
+        self.button.clicked.connect(self.jogo)
+
+    def pedra(self):
+        self.jogo("pedra")
+
+    def papel(self):
+        self.jogo("papel")
+
+    def tesoura(self):
+        self.jogo("tesoura")
+
+    def jogo(self, escolha):
+        choices = ["pedra", "papel", "tesoura"]
+        jogada = random.choice(choices)
+
+        result = self.resultado(escolha, jogada)
+
+        self.label.setText(f"Você escolheu: {escolha}\n"
+                           f"O computador escolheu: {jogada}\n"
+                           f"Resultado: {result}")
+
+    def resultado(self, escolha, jogada):
+        if escolha == jogada:
+            return "Empate"
+        elif (escolha == "pedra" and jogada == "tesoura") or \
+                (escolha == "papel" and jogada == "pedra") or \
+                (escolha == "tesoura" and jogada == "papel"):
+            return "Você venceu!"
+        else:
+            return "Você perdeu!"
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
