407f971f49b8ad4b4384349fa6fb2cb2f2afd987
julinha <114812014+juliaRobertav@users.noreply.github.com>
2023-06-21 11:09:30 -0300

    Update jogo2.py

--- jogo2.py
+++ jogo2.py
@@ -1,68 +1,58 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel, QVBoxLayout, QWidget
+import random
 from PyQt5.QtGui import QFont
+from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget, QLineEdit, QPushButton, QLabel
 
-class RPSGame(QMainWindow):
+
+class Jokenpo(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("Pedra, Papel e Tesoura")
-        self.setGeometry(200, 200, 300, 200)
 
         self.label = QLabel("Escolha uma opção:")
         self.label.setFont(QFont("Arial", 12))
 
-        self.button_rock = QPushButton("Pedra")
-        self.button_paper = QPushButton("Papel")
-        self.button_scissors = QPushButton("Tesoura")
-
         layout = QVBoxLayout()
+
+        self.line_edit = QLineEdit()
+        self.button = QPushButton("JOGAR")
+
         layout.addWidget(self.label)
-        layout.addWidget(self.button_rock)
-        layout.addWidget(self.button_paper)
-        layout.addWidget(self.button_scissors)
+        layout.addWidget(self.line_edit)
+        layout.addWidget(self.button)
 
         widget = QWidget()
         widget.setLayout(layout)
         self.setCentralWidget(widget)
 
-        self.button_rock.clicked.connect(self.play_rock)
-        self.button_paper.clicked.connect(self.play_paper)
-        self.button_scissors.clicked.connect(self.play_scissors)
+        self.button.clicked.connect(self.escolha_jogador)
 
-    def play_rock(self):
-        self.play("rock")
+    def escolha_jogador(self):
+        escolha = self.line_edit.text().lower()
+        self.jogo(escolha)
 
-    def play_paper(self):
-        self.play("paper")
+    def jogo(self, escolha):
+        choices = ["pedra", "papel", "tesoura"]
+        jogada = random.choice(choices)
 
-    def play_scissors(self):
-        self.play("scissors")
+        result = self.resultado(escolha, jogada)
 
-    def play(self, player_choice):
-        import random
-
-        choices = ["rock", "paper", "scissors"]
-        computer_choice = random.choice(choices)
-
-        result = self.get_result(player_choice, computer_choice)
-
-        self.label.setText(f"Você escolheu: {player_choice}\n"
-                           f"O computador escolheu: {computer_choice}\n"
+        self.label.setText(f"Você escolheu: {escolha}\n"
+                           f"O computador escolheu: {jogada}\n"
                            f"Resultado: {result}")
 
-    def get_result(self, player_choice, computer_choice):
-        if player_choice == computer_choice:
+    def resultado(self, escolha, jogada):
+        if escolha == jogada:
             return "Empate"
-        elif (player_choice == "rock" and computer_choice == "scissors") or \
-             (player_choice == "paper" and computer_choice == "rock") or \
-             (player_choice == "scissors" and computer_choice == "paper"):
+        elif (escolha == "pedra" and jogada == "tesoura") or \
+                (escolha == "papel" and jogada == "pedra") or \
+                (escolha == "tesoura" and jogada == "papel"):
             return "Você venceu!"
         else:
             return "Você perdeu!"
 
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    game = RPSGame()
-    game.show()
-    sys.exit(app.exec_())
 
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    janela = Jokenpo()
+    janela.show()
+    sys.exit(app.exec_())
